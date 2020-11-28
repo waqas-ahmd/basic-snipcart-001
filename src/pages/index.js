5bd8e126b9f9c1beb56322d612510de77c8aacff
waqas-ahmd <65waqas@gmail.com>
2020-11-28 16:40:03 +0500

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      Hello world!
+      <button
+        class="snipcart-add-item"
+        data-item-id="starry-night"
+        data-item-price="35.99"
+        data-item-url="/paintings/starry-night"
+        data-item-description="High-quality Men Shoe, lustrous Shine"
+        data-item-image="https://cdn.shopify.com/s/files/1/0104/5757/9583/products/OSCO-Men-Dress-Shoes-Men-Formal-Shoes-Leather-Luxury-Fashion-Wedding-Shoes-Men-Business-Casual-Oxford.jpg?v=1575045878"
+        data-item-name="Men Black Shoe"
+      >
+        Add to cart
+      </button>
+    </div>
+  )
 }
