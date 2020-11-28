30fc8e3d703d19c927790788330540cbef6c3dba
waqas-ahmd <65waqas@gmail.com>
2020-11-28 16:48:09 +0500

    url edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         class="snipcart-add-item"
         data-item-id="starry-night"
         data-item-price="35.99"
-        data-item-url="/paintings/starry-night"
+        data-item-url="/"
         data-item-description="High-quality Men Shoe, lustrous Shine"
         data-item-image="https://cdn.shopify.com/s/files/1/0104/5757/9583/products/OSCO-Men-Dress-Shoes-Men-Formal-Shoes-Leather-Luxury-Fashion-Wedding-Shoes-Men-Business-Casual-Oxford.jpg?v=1575045878"
         data-item-name="Men Black Shoe"
